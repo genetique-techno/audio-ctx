41179094aae06aa0aaa5a3b0a8adf803c6ad8df8
Aaron Todd <atodd@onetwosee.com>
2016-09-28 22:31:06 -0400

    Frequency readout

--- app/script.js
+++ app/script.js
@@ -1,11 +1,33 @@
+var audio = document.querySelector( 'audio' );
+var readout = document.getElementById('readout');
+var audioCtx = new ( window.AudioContext || window.webkitAudioContext )();
+var analyser = audioCtx.createAnalyser();
+var source;
 
-navigator.getUserMedia({ audio: true }, success, error );
+navigator.getUserMedia({ audio: true }, (stream) => {
 
-function success( stream ) {
-  var audio = document.querySelector( 'audio' );
-  audio.src = window.URL.createObjectURL( stream );
-}
+  source = audioCtx.createMediaStreamSource(stream);
+  source.connect(analyser);
 
-function error( err ) {
+}, (err) => {
   console.log( 'Error occurred: ', err );
+} );
+
+analyser.fftSize = 32;
+var dataArray = new Uint8Array( analyser.frequencyBinCount );
+
+analyser.getByteFrequencyData( dataArray );
+analyser.minDecibels = -90;
+analyser.maxDecibels = -10;
+analyser.smoothingTimeConstant = 0.85;
+
+function draw() {
+
+  drawVisual = requestAnimationFrame( draw );
+
+  analyser.getByteFrequencyData( dataArray );
+  readout.innerHTML = dataArray;
+
 }
+
+draw();
