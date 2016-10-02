c4ee82658ba2a685492a4911f189176dc98ceb77
Aaron Todd <atodd@onetwosee.com>
2016-10-02 19:38:48 -0400

    Constructors can be passed into the persistant things interface

--- app/PersistantThings.js
+++ app/PersistantThings.js
@@ -1,18 +1,17 @@
-function PersistantThings( ctx, analyser, thresholdVal ) {
+function PersistantThings( ctx, analyser, ThingConstructor, thresholdVal ) {
   if ( !ctx ) { return console.log( 'ERROR: No context passed to PersistantThings' ) }
   if ( !analyser ) { return console.log( 'ERROR: No analyser passed to PersistantThings' ) }
 
-  var width = ctx.canvas.width;
-  var height = ctx.canvas.height;
-
   return {
     drawStack: [],
+    width: ctx.canvas.width,
+    height: ctx.canvas.height,
 
     draw: function( delta ) {
 
       analyser.__array.forEach( ( amp, bin, arr ) => {
         if ( amp > thresholdVal ) {
-          this.drawStack.push( new this.Thing( arr, bin ) );
+          this.drawStack.push( new this.Thing( ctx, arr, bin ) );
         }
       });
 
@@ -32,36 +31,37 @@ function PersistantThings( ctx, analyser, thresholdVal ) {
       });
     },
 
-    Thing: function( inArr, bin ) {
-      var inValue = inArr[bin];
-      var xPos = bin/inArr.length*width + 1/(inArr.length*2)*width;
-      var yPos = 0;
-      var radius = inValue / 5;
-      var color = 'rgb('+ 10 +', '+ Math.min( parseInt( inValue*0.80 ), 255 ) +', ' + Math.min( parseInt( inValue*1.5 ), 255 ) + ')';
-      var t = 0;
-
-      return {
-        clear: false,
-        animate: function() {
-
-          yPos += 1.2*t^2;
-          t++;
-
-          ctx.moveTo( xPos, yPos );
-          ctx.beginPath();
-          ctx.arc( xPos, yPos, radius, Math.PI*2, false );
-          ctx.stroke();
-          ctx.fillStyle = color;
-          ctx.fill();
-
-          if ( yPos > height + radius ) {
-            this.clear = true;
-          }
-        }
-
-      }
-    }
+    Thing: ThingConstructor.bind( this )
   }
 
 }
 
+function CircleConstructor( ctx, inArr, bin ) {
+  var inValue = inArr[bin];
+  var xPos = bin/inArr.length*ctx.canvas.width + 1/(inArr.length*2)*ctx.canvas.width;
+  var yPos = 0;
+  var radius = inValue / 5;
+  var color = 'rgb('+ 10 +', '+ Math.min( parseInt( inValue*0.80 ), 255 ) +', ' + Math.min( parseInt( inValue*1.5 ), 255 ) + ')';
+  var t = 0;
+
+  return {
+    clear: false,
+    animate: function() {
+
+      yPos += 1.2*t^2;
+      t++;
+
+      ctx.moveTo( xPos, yPos );
+      ctx.beginPath();
+      ctx.arc( xPos, yPos, radius, Math.PI*2, false );
+      ctx.stroke();
+      ctx.fillStyle = color;
+      ctx.fill();
+
+      if ( yPos > ctx.canvas.height + radius ) {
+        ctx.canvas.clear = true;
+      }
+    }
+
+  }
+}
